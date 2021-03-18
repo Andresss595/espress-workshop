9990749f3d62058b2a951df297937b5514324075
Andrés <andresssjimeeli@gmail.com>
2021-03-17 21:43:13 -0600

    added json database pokedex

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json'); //exportar el archivo json y si esta entre {} se exporta todo como tal
 
 
-console.log("Hola Mundo!");
 /*
 Los verbos HTTP
 GET - OBTENER ALGO
@@ -10,12 +10,46 @@ POST -
 PATCH - cambia solo unos elementos 
 PUT - cambia todos lo elemntos
 DELETE
+
+Agregar nodemon de manera global para que se valla actualizando
+
+agregar los datos de bugs, homepage y en el reporitorio
 */
 app.get("/", (req, res, next)=>{
-    res.send("Bienvenido!");
-
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
 });
 
-app.listen(3000, () => {
+app.get('/pokemon/all',(req, res, next)=>{
+    res.status(200);
+    res.send(pokemon);
+});
+/*importa el orden de las rutas
+
+RegExp es una serie de caracteres, somete a evaluacion una cadena que le hallamos metido [rango de digitos] {cantidad de digitos}*/
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>{
+    const id = req.params.id -1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++){
+        if(pokemon[i].name == name ){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encotrado");
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
