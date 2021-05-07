5a757ac0e723f206ea9c72d347ca7aad737746d1
Andrés <andresssjimeeli@gmail.com>
2021-05-06 21:59:57 -0500

    added UI part 3!

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const notFoud = require('./middleware/notFound');
 const index = require('./middleware/index');
 const cors = require('./middleware/cors');
  
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,8 +42,6 @@ app.get("/", index);
 app.use("/user", user);
 app.use(auth);
 app.use("/pokemon", pokemon); //no hay preferencias, solo considerar que se lee de arriba a abajo
-
-
 app.use(notFoud);
 
 app.listen(process.env.PORT || 3000, () => {
